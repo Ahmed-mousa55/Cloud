489c64f74e03a3a9e59e297833e61cd97d0e0e0c
ahmed-mousa55 <ahmed.mousa4499@gmail.com>
2022-04-07 00:24:05 +0200

    no desc

---
+++ script.js
@@ -0,0 +1,76 @@
+// var friends = new Array("Ahme","Mohamed");
+// friends[0] = "Ahmed";
+
+// var friend = ["Ahmed", "Mohamed", "Ibrahim", "Zahra", "Youseff", "Mahmoud", "saeed"];
+
+// var workFriend = ["AHWork", "Mhwork", "YoWork"];
+
+// // IndexOf(value to search, starting index);
+
+// var specailFriend = friend.lastIndexOf("Zahra",4);
+// console.log(specailFriend);
+
+// var E=["Noran","Esraa"]
+// var All = friend.concat(workFriend,E);
+// console.log(All);
+
+// var sliced = friend.slice(-7, -2);
+// console.log(sliced);
+
+// friend.sort();
+// console.log(friend);
+// friend.reverse();
+// console.log(friend);
+
+
+
+// console.log(friend.join(" And "));
+
+// friend[friend.length] = "Ahmed6";
+// console.log(friend);
+
+// friend.push("Ahmed9");
+// friend.unshift("Ahm");
+// console.log(friend);
+
+// friend.splice(1, 3, "Mohamed", "Mahmoud")
+// console.log(friend);
+
+// var firstItem = friend.shift();
+// console.log(firstItem);
+// var lastItem = friend.pop();
+// console.log(lastItem);
+
+// var date = new Date();
+// console.log(date.toLocaleTimeString());
+
+
+// var msg = [
+//     "msg1",
+//     "msg2",
+//     "msg3",
+//     "msg4"
+// ];
+
+// var x=msg.slice(-4, -2);
+// console.log(x);
+
+// if (msg.constructor === Array) {
+//     console.log("Yeah its");
+// }
+// else {
+//     console.log("NO its not ");
+// }
+// // msg.length=2
+
+// console.log(msg);
+// console.log(msg.toString());
+
+// var date = new Date();
+// console.log(date.toString());
+// console.log(date.toLocaleString());
+
+// var mystring = "Hello I love Javascript And i love php";
+// var s = mystring.lastIndexOf("love",20);
+// console.log(s);
+
